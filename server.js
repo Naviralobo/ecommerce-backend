054918f91df6d4b0220bdf270def44be1a9f5bb8
Naviralobo <majel.josephine>
2026-07-01 14:15:46 +0530

    chore: install express and initiate

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
+const app = express();
 
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Welcome to the homepage!");
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Page not found");
-  }
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the homepage!" });
+  // res.send("Welcome to the homepage!");
 });
 
-server.listen(3000, () => {
+app.use((req, res) => {
+  res.status(404).json({ error: "Page not found" });
+  //res.status(404).send("Page not found");
+});
+
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+// nodejs
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+
+//   if (req.url === "/") {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.end("Welcome to the homepage!");
+//   } else {
+//     res.writeHead(404, { "Content-Type": "text/plain" });
+//     res.end("Page not found");
+//   }
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
