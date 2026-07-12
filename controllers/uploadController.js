543ee9920fc30d676550fe5f37cee32747fe07b7
Naviralobo <majel.josephine>
2026-07-12 05:43:58 +0530

    feat :upload file with expiry

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,5 +1,6 @@
 const { uploadFile } = require("../services/s3Service");
-const FileModel = require("../models/file"); 
+const FileModel = require("../models/file");
+const { getSignedFileUrl } = require("../services/s3Service");
 
 exports.upload = async (req, res) => {
   try {
@@ -10,9 +11,14 @@ exports.upload = async (req, res) => {
     const key = `${Date.now()}-${file.originalname}`; // Generate a unique key for the file
 
     await uploadFile(file, key);
-    const fileUrl = `${process.env.AWS_BUCKET_NAME}/${key}`;
-    const saveFile = await FileModel.create({ key, fileUrl });
-    res.status(200).json({ message: "File uploaded successfully" });
+    // const fileUrl = `${process.env.AWS_BUCKET_URl}/${key}`;
+    // const saveFile = await FileModel.create({ key, fileUrl });
+    // Instead of doing the above do the below
+    const saveFile = await FileModel.create({ key });
+    const fileUrl = await getSignedFileUrl(key, 20); // Generate a signed URL for the uploaded file, valid for 1 hour (3600 seconds)
+    res
+      .status(200)
+      .json({ message: "File uploaded successfully", key, fileUrl });
   } catch (error) {
     console.error("Error uploading file:", error);
     console.log(error);
--- services/s3Service.js
+++ services/s3Service.js
@@ -1,7 +1,15 @@
 //services are used to handle the business logic of the application. In this case, the s3Service.js file is responsible for handling file uploads and deletions to and from an Amazon S3 bucket.
 const s3Client = require("../config/s3Client");
-const { PutObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const {
+  PutObjectCommand,
+  DeleteObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
 
+// this filename make same image name to be uploaded to s3 bucket, so we need to make unique name for each image, so we can use timestamp or uuid to make unique name for each image. Here we are using timestamp to make unique name for each image.
+//1783584642204-WIN_20260331_16_25_58_Pro.jpg
+//1783584642204 - This is the timestamp and remaining will be same as the name of the local file
 const uploadFile = async (file, fileName) => {
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
@@ -22,4 +30,15 @@ const deleteFile = async (fileName) => {
   return await s3Client.send(new DeleteObjectCommand(params));
 };
 
-module.exports = { uploadFile, deleteFile };
+const getSignedFileUrl = async (fileName, expiresIn = 10) => {
+  const signedUrlParams = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: fileName,
+    Expires: expiresIn,
+  };
+  return await getSignedUrl(s3Client, new GetObjectCommand(signedUrlParams), {
+    expiresIn,
+  });
+};
+
+module.exports = { uploadFile, deleteFile, getSignedFileUrl };
