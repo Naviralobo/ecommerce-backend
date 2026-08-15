a2c50aac26f7ab82f743d1d9d9421a4d441411c5
Naviralobo <majel.josephine>
2026-08-15 17:33:26 +0530

    fix: allow refresh token cookie for cross-site requests

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -27,7 +27,7 @@ export const login = asyncHandler(async (req, res) => {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: false,
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
